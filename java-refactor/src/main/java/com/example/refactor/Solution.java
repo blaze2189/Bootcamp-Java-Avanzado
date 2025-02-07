965b32c6e241c2c586c46e69e2ef353d90b0acd7
blaze2189 <jrgalc2189@gmail.com>
2025-02-06 22:56:47 -0600

    Solution
    update: cambio de declaracion de objeto, utilizando la interfaz creada

--- java-refactor/src/main/java/com/example/refactor/Solution.java
+++ java-refactor/src/main/java/com/example/refactor/Solution.java
@@ -1,10 +1,11 @@
 package com.example.refactor;
 
+import com.example.refactor.service.SongProcessor;
 import com.example.refactor.service.SpotifySongProcessor;
 
 public class Solution {
     public static void main(String... args) {
-        SpotifySongProcessor spotifySongProcessor = new SpotifySongProcessor();
+        SongProcessor spotifySongProcessor = new SpotifySongProcessor();
         spotifySongProcessor.processSongs();
     }
 }
