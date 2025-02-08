817008e4632d6705ed6e3e43d8a1f38a1d78da26
blaze2189 <jrgalc2189@gmail.com>
2025-02-08 13:02:48 -0600

    Sealed classes
    add: agrega funcional selaed class a la interfaz que define le mètodo para procesar canciones

--- java-refactor/src/main/java/com/example/refactor/service/SongProcessor.java
+++ java-refactor/src/main/java/com/example/refactor/service/SongProcessor.java
@@ -3,8 +3,17 @@ package com.example.refactor.service;
 /**
  * Creaciòn de interfaz para especificar comportamientos
  * de un procesador de canciones
+ *
+ * Utilización de sealed clases, solo las clases que
+ * tengan como objetivo procesar canciones deben
+ * implmentar esta interfaz y por lo tanto serán
+ * especificadas en el permits
  * */
+<<<<<<< HEAD
 public interface SongProcessor {
+=======
+public sealed interface SongProcessor permits  SpotifySongProcessor{
+>>>>>>> 81125c4 (Sealed classes)
     void processSongs();
 
     void processSongs(String pathToFile);
