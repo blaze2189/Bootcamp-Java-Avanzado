0e706492a1501b9136590afb8e8880d5352f30f6
blaze2189 <jrgalc2189@gmail.com>
2025-02-09 15:27:04 -0600

    Paquetes
    modify: package para clases utilitarias

--- java-refactor/src/main/java/com/example/refactor/util/ExampleFileUtils.java
+++ java-refactor/src/main/java/com/example/refactor/util/ExampleFileUtils.java
@@ -1,14 +1,13 @@
-package com.example.refactor.service;
+package com.example.refactor.util;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
 import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,7 +18,7 @@ public class ExampleFileUtils {
     public static JSONObject getJsonFromFile(File inputSource) {
         JSONParser parser = new JSONParser();
         //agregando try-resources para cerrar el recurso FileReader
-        try(FileReader fileReader = new FileReader(inputSource)) {
+        try (FileReader fileReader = new FileReader(inputSource)) {
             return (JSONObject) parser.parse(fileReader);
         } catch (IOException | ParseException e) {
             //Removiendo printstackrace
@@ -35,7 +34,7 @@ public class ExampleFileUtils {
 
         //actualización a Optional
         URL optionalURL = Optional.ofNullable(classLoader.getResource(fileName)).
-                orElseThrow(()->new IllegalArgumentException("Missing file"));
+                orElseThrow(() -> new IllegalArgumentException("Missing file"));
 
         return new File(optionalURL.getFile());
     }
